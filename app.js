0f1da8b68d0194df2dd7d37353a3d7c65345e5f3
Jacob Dalton <jacob.dalton@sportngin.com>
2014-05-04 15:12:47 -0500

    basic workbench

---
+++ app.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var angular = require('angular');
+var _ = require('underscore');
+
+var app = angular.module('app', [])
+.controller('trollController', function($scope) {
+
+});
---
+++ server.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = new express();
+
+app.use(express.json());
+
+app.use(express.static(__dirname + '/public'));
+
+var port = Number(process.env.PORT || 3003);
+app.listen(port);
+
+console.log("Server is listening on port: " + port);
