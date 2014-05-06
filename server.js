438c1b5d66701ee89afcb18e9ddaa633ba342202
Jacob Dalton <jacob.dalton@sportngin.com>
2014-05-06 15:20:28 -0500

    create svg workbench file with examples of using svg

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ app.use(express.json());
 
 app.use(express.static(__dirname + '/public'));
 
-var port = Number(process.env.PORT || 3003);
+var port = Number(process.env.PORT || 5000);
 app.listen(port);
 
 console.log("Server is listening on port: " + port);
